0234db8fb2dc90c181e537780f28ab86d66ef473
Mexican_Man <sombrero13nn@gmail.com>
2019-11-21 20:04:15 +0300

    Static draw finished

--- WebSPAClient/ClientApp/src/components/game/Game.tsx
+++ WebSPAClient/ClientApp/src/components/game/Game.tsx
@@ -1,4 +1,4 @@
-﻿import React, { Component, ChangeEvent, FormEvent } from 'react';
+﻿import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as GameReducer from '../../store/game/GameReducer';
 import { ApplicationState } from '../../store';
@@ -8,6 +8,11 @@ import BaseGameWindow from './BaseGameWindow';
 import { faHeart } from '@fortawesome/free-solid-svg-icons/faHeart';
 import { faStarOfLife } from '@fortawesome/free-solid-svg-icons/faStarOfLife';
 import { faWindowMinimize } from '@fortawesome/free-regular-svg-icons/faWindowMinimize';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons/faEllipsisV';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons/faArrowUp';
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons/faArrowDown';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons/faArrowLeft';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as RoomSymbolTypes from './RoomSymbolTypes';
 
@@ -70,7 +75,7 @@ class Game extends Component<GameProps> {
     render() {
         return (
             <BaseGameWindow>
-                <div className="row">
+                <div id="gameRow" className="row no-gutters">
                     <div id="fieldSide" className="col-6">
                         <div id="field" className="text-white bg-dark m-2">
                             <div id="monsterRow" className="row ml-1 mr-1 pt-2">
@@ -78,7 +83,7 @@ class Game extends Component<GameProps> {
                                     <span className="mr-1">?/?</span> <strong className="text-white">M</strong>
                                 </div>
                             </div>
-                            <div id="room" className="row">
+                            <div id="room" className="row no-gutters">
                                 <table className="room-symbol offset-3 col-6">
                                     <tbody>
                                         {this.roomMap.map((row, index) => (
@@ -99,6 +104,58 @@ class Game extends Component<GameProps> {
                                     <span className="float-right">Nick</span>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                    <div id="controllerSide" className="col-6 pr-1">
+                        <div id="menuRow" className="row m-2 p-1 no-gutters">
+                            <div className="col-6">
+                                <button type="button" className="btn btn-primary" onClick={() => { }}>
+                                    <FontAwesomeIcon icon={faEllipsisV} className="mr-1" /> Menu
+                                </button>
+                            </div>
+                            <div className="col-6">
+                                <button type="button" className="btn btn-primary float-right" onClick={() => { }}>
+                                    Leaderboard
+                                </button>
+                            </div>
+                        </div>
+                        <div id="treasureRow" className="row m-2 p-1 no-gutters justify-content-center">
+                            <button type="button" className="btn btn-warning text-white col-md-5 col-sm-7" onClick={() => { }}>
+                                Pickup Treasure
+                            </button>
+                        </div>
+                        <div id="atkMonsterRow" className="row m-2 p-1 no-gutters justify-content-center">
+                            <button type="button" className="btn btn-danger text-white col-md-5 col-sm-7" onClick={() => { }}>
+                                Attack Monster
+                            </button>
+                        </div>
+                        <div id="upRow" className="row m-2 pt-1 no-gutters justify-content-center">
+                            <button type="button" className="btn btn-success text-white" onClick={() => { }}>
+                                <FontAwesomeIcon icon={faArrowUp} />
+                            </button>
+                        </div>
+                        <div id="ldrRow" className="row mt-2 ml-2 mr-2 no-gutters justify-content-center">
+                            <button type="button" className="btn btn-success text-white ml-1 mr-1" onClick={() => { }}>
+                                <FontAwesomeIcon icon={faArrowLeft} />
+                            </button>
+                            <button type="button" className="btn btn-success text-white ml-1 mr-1" onClick={() => { }}>
+                                <FontAwesomeIcon icon={faArrowDown} />
+                            </button>
+                            <button type="button" className="btn btn-success text-white ml-1 mr-1" onClick={() => { }}>
+                                <FontAwesomeIcon icon={faArrowRight} />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div id="logRow" className="row no-gutters">
+                    <div className="row no-gutters ml-2"><span className="col-12">Text log:</span></div>
+                    <div id="log" className="row no-gutters border rounded border-dark overflow-auto col-12">
+                        <div id="logText" className="ml-2">
+                            2019-11-21 13:34 You came to the room #3 discovered by Nickname!<br/>
+                            2019-11-21 13:35 You created a new room #10!<br />
+                            2019-11-21 13:36 You hit monster and it hit you back!<br />
+                            2019-11-21 13:37 You hit monster and it died!<br />
+                            2019-11-21 13:39 You pickuped treasure!<br />
                         </div>
                     </div>
                 </div>
